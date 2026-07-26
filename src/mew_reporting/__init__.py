77b9f3bf594f907c58ee4c25a35fd2b010164006
Tom Bombadil <64350838+EarwainBanadar@users.noreply.github.com>
2026-07-26 20:55:19 +0200

    refactor: define explicit reporting package API

--- src/mew_reporting/__init__.py
+++ src/mew_reporting/__init__.py
@@ -1,2 +1,10 @@
-from .models import ReportMetadata, ReportingError
+"""Reporting engine public API."""
+
 from .engine import ReportingEngine
+from .models import ReportMetadata, ReportingError
+
+__all__ = [
+    "ReportMetadata",
+    "ReportingEngine",
+    "ReportingError",
+]
