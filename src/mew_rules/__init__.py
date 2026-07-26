55d8b3b7922f65c5720911b51ddb1828152d37af
Tom Bombadil <64350838+EarwainBanadar@users.noreply.github.com>
2026-07-26 20:54:45 +0200

    refactor: define explicit rules package API

--- src/mew_rules/__init__.py
+++ src/mew_rules/__init__.py
@@ -1,15 +1,56 @@
 """AP9.3.1 Rule Framework."""
-from .model import (
-    RuleCategory, RuleSeverity, RuleStatus, RuleScope,
-    RuleDefinition, RuleFinding, RuleExecutionResult, RuleContext
-)
-from .base import Rule, FunctionalRule
-from .registry import RuleRegistry, DuplicateRuleIdError, UnknownRuleError
 
-from .kb_repository import *
-from .kb_loader import KnowledgeBaseLoader
-
-from .evaluator import RuleEvaluator, EvaluationPolicy, EvaluationSummary
+from .base import FunctionalRule, Rule
 from .dispatcher import RuleDispatcher
-
+from .evaluator import EvaluationPolicy, EvaluationSummary, RuleEvaluator
+from .kb_loader import KnowledgeBaseLoader
+from .kb_repository import (
+    DuplicateKnowledgeRule,
+    InvalidKnowledgeRule,
+    KnowledgeBaseError,
+    KnowledgeBaseRepository,
+    KnowledgeRuleRecord,
+    UnsupportedKnowledgeFormat,
+    parse_semver,
+)
+from .model import (
+    RuleCategory,
+    RuleContext,
+    RuleDefinition,
+    RuleExecutionResult,
+    RuleFinding,
+    RuleScope,
+    RuleSeverity,
+    RuleStatus,
+)
+from .registry import DuplicateRuleIdError, RuleRegistry, UnknownRuleError
 from .standard_rules import create_standard_registry
+
+__all__ = [
+    "DuplicateKnowledgeRule",
+    "DuplicateRuleIdError",
+    "EvaluationPolicy",
+    "EvaluationSummary",
+    "FunctionalRule",
+    "InvalidKnowledgeRule",
+    "KnowledgeBaseError",
+    "KnowledgeBaseLoader",
+    "KnowledgeBaseRepository",
+    "KnowledgeRuleRecord",
+    "Rule",
+    "RuleCategory",
+    "RuleContext",
+    "RuleDefinition",
+    "RuleDispatcher",
+    "RuleEvaluator",
+    "RuleExecutionResult",
+    "RuleFinding",
+    "RuleRegistry",
+    "RuleScope",
+    "RuleSeverity",
+    "RuleStatus",
+    "UnknownRuleError",
+    "UnsupportedKnowledgeFormat",
+    "create_standard_registry",
+    "parse_semver",
+]
